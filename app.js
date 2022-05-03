b0d2b6a67cee7316f7cb8d13c61330e3aa855aab
com <x7788@naver.com>
2022-05-03 18:17:37 +0900

    🎉 포트폴리오 세팅

---
+++ app.js
@@ -0,0 +1,14 @@
+let scrollTop = 0
+let bar
+
+window.onload = function () {
+  bar = document.querySelector('.bar')
+}
+
+window.addEventListener('scroll', (e) => {
+  scrollTop = document.documentElement.scrollTop
+  let per = Math.ceil(
+    (scrollTop / (document.body.scrollHeight - window.outerHeight)) * 100
+  )
+  bar.style.width = per + '%'
+})
